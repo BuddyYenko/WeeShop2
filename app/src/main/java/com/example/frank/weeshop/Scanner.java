cdfb31eb6d0a0776b3a487b7e66c32de55d12748
buddyyenko@gmail.com <BuddyYenko>
2018-09-05 16:37:11 +0200

    Scanner

--- app/src/main/java/com/example/frank/weeshop/Scanner.java
+++ app/src/main/java/com/example/frank/weeshop/Scanner.java
@@ -1,13 +1,90 @@
 package com.example.frank.weeshop;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Scanner extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Scanner extends AppCompatActivity implements View.OnClickListener {
+    private RequestQueue queue;
+
+    //view objects
+    ImageView buttonScan;
+    public String product_id, user_id;
+
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+
+        user_id = preferences.getString("user_id", "");
+
+        queue = Volley.newRequestQueue(this);
+        buttonScan = findViewById(R.id.buttonScanner);
+        qrScan = new IntentIntegrator(this);
+        buttonScan.setOnClickListener(this);
+    }
+    //Getting results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null)
+        {
+            if (result.getContents() == null)
+            {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                try
+                {
+                    JSONObject obj = new JSONObject(result.getContents());
+                    product_id = obj.getString("product_id");
+
+                    SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+
+                    String productIDSession = preferences.getString(product_id + "data", product_id);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("product_id", productIDSession);
+                    editor.putString("user_id", user_id);
+                    editor.commit();
+
+                    Intent scanHome = new Intent(Scanner.this, ScanHome.class);
+                    startActivity(scanHome);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    @Override
+    public void onClick(View view)
+    {
+        qrScan.initiateScan();
     }
 }
